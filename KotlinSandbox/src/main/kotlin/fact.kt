1539f3da83fcbacde4d2606822edb07f331e3ba4
Alex Baryzhikov <aleksiarts@gmail.com>
2018-10-14 15:52:30 +0300

    Style

--- KotlinSandbox/src/main/kotlin/fact.kt
+++ KotlinSandbox/src/main/kotlin/fact.kt
@@ -1,13 +1,13 @@
 interface IFact {
-  operator fun invoke(self: IFact, n: Int): Int
+    operator fun invoke(self: IFact, n: Int): Int
 }
 
 fun fact(n: Int) = { self: IFact -> self(self, n) }(object : IFact {
-  override fun invoke(self: IFact, n: Int) = if (n == 1) 1 else n * self(self, n - 1)
+    override fun invoke(self: IFact, n: Int): Int = if (n == 1) 1 else n * self(self, n - 1)
 })
 
 fun main(args: Array<String>) {
-  assert(fact(5) == 120)
-  assert(fact(8) == 40320)
-  println("OK")
+    assert(fact(5) == 120)
+    assert(fact(8) == 40320)
+    println("OK")
 }
