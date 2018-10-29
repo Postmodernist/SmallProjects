8ad539edf6132285932abf6bdaba6c7c480a5fc9
Alex Baryzhikov <aleksiarts@gmail.com>
2018-10-29 23:35:54 +0300

    Feat: IO.async()

--- ArrowSandbox/src/main/kotlin/hello.kt
+++ ArrowSandbox/src/main/kotlin/hello.kt
@@ -1,11 +1,29 @@
 import arrow.effects.IO
+import arrow.effects.async
 import arrow.effects.fix
 import arrow.effects.monad
 import arrow.typeclasses.binding
+import kotlinx.coroutines.experimental.Dispatchers
 
 fun main(args: Array<String>) {
-    val a = IO.monad().binding {
-        1
+    val res = IO.monad().binding {
+        val a = IO.invoke { 1 }.bind()
+        a + 1
     }.fix().unsafeRunSync()
-    println(a)
+    println(res)
+
+    IO.async().run {
+        binding {
+            // In current thread
+            val id = just(1).bind()
+            continueOn(Dispatchers.Default)
+
+            // In Default dispatcher now!
+            val result = IO.invoke { id * 10 }.bind()
+//            continueOn(Dispatchers.Main)
+
+            // In UI now!
+            println(result)
+        }
+    }.fix().unsafeRunSync()
 }
