2a3219522cf742ad02a4e2d5691e649676019125
Alex Baryzhikov <aleksiarts@gmail.com>
2018-11-01 22:59:42 +0300

    Feature: MapSet

---
+++ KotlinSandbox/src/main/kotlin/mapset.kt
@@ -0,0 +1,42 @@
+interface MapSet {
+    operator fun <E : Element> get(key: Key<E>): E?
+
+    interface Key<E : Element>
+
+    interface Element : MapSet {
+        /** A key of this MapSet element. */
+        val key: Key<*>
+
+        override operator fun <E : Element> get(key: Key<E>): E? =
+                @Suppress("UNCHECKED_CAST")
+                if (this.key == key) this as E else null
+    }
+}
+
+class MapSetImpl : MapSet {
+
+    private val mutableMap = mutableMapOf<MapSet.Key<*>, MapSet.Element>()
+
+    override fun <E : MapSet.Element> get(key: MapSet.Key<E>): E? {
+        @Suppress("UNCHECKED_CAST")
+        return mutableMap[key] as E?
+    }
+
+    fun put(key: MapSet.Key<*>, element: MapSet.Element) {
+        mutableMap[key] = element
+    }
+
+}
+
+class User(val name: String) : MapSet.Element {
+    companion object Key : MapSet.Key<User>
+
+    override val key = Key
+    override fun toString() = "User(name=$name)"
+}
+
+fun main(args: Array<String>) {
+    val mapSet = MapSetImpl()
+    mapSet.put(User.Key, User("Tom"))
+    println(mapSet[User.Key])
+}
