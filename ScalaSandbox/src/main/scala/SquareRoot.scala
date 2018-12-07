6e19968268bc4d98e0bd5e913b3e6fe4838dd0e7
Alex Baryzhikov <aleksiarts@gmail.com>
2018-12-07 07:55:51 +0300

    Refactor: Sqrt

--- ScalaSandbox/src/main/scala/SquareRoot.scala
+++ ScalaSandbox/src/main/scala/SquareRoot.scala
@@ -1,15 +1,18 @@
 object SquareRoot extends App {
-    def sqrtIter(guess: Double, x: Double): Double =
-        if (isGoodEnough(guess, x)) guess
-        else sqrtIter(improve(guess, x), x)
 
-    def improve(guess: Double, x: Double) =
-        (guess + x / guess) / 2
+    def sqrt(x: Double) = {
+        def sqrtIter(guess: Double): Double =
+            if (isGoodEnough(guess)) guess
+            else sqrtIter(improve(guess))
 
-    def isGoodEnough(guess: Double, x: Double) =
-        Math.abs(guess * guess - x) < 0.001
+        def improve(guess: Double) =
+            (guess + x / guess) / 2
 
-    def sqrt(x: Double) = sqrtIter(1.0, x)
+        def isGoodEnough(guess: Double) =
+            Math.abs(guess * guess - x) < 0.001
+
+        sqrtIter(1.0)
+    }
 
     println(sqrt(2))
 }
