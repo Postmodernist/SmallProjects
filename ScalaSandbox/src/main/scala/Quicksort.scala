06fcd2b5e6c88450a04c881253619765cf4bc5c3
Alex Baryzhikov <aleksiarts@gmail.com>
2018-12-20 22:52:33 +0300

    Feature: Qsort

---
+++ ScalaSandbox/src/main/scala/Quicksort.scala
@@ -0,0 +1,14 @@
+object Quicksort extends App {
+
+  def qsort[T](l: List[T])(implicit ord: Ordering[T]): List[T] = l match {
+    case Nil => Nil
+    case x :: xs =>
+      val ys = xs filter (ord lteq(_, x))
+      val zs = xs filter (ord gt(_, x))
+      qsort(ys) ::: List(x) ::: qsort(zs)
+  }
+
+  println(qsort(List(4, 3, 2, 1)))
+  println(qsort(List('b', 'z', 'd', 'x', 'k')))
+  println(qsort(List("foo", "bar", "baz", "qux", "quux", "quuz", "corge", "grault", "garply", "waldo")))
+}
