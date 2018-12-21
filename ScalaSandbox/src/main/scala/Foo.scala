ad4171d2becfe1b0aa717849a22180b9aa638bee
Alex Baryzhikov <aleksiarts@gmail.com>
2018-12-21 21:58:18 +0300

    Feature: Foo

---
+++ ScalaSandbox/src/main/scala/Foo.scala
@@ -0,0 +1,18 @@
+object Foo extends App {
+  val double: Int => Int = _ * 2
+  val quad = double compose double
+  (quad andThen println) (3)
+
+  println(List(1, 2, 3).product)
+
+  val xs = Stream from 1
+  (xs take 10) foreach print
+  println
+
+  val average: List[Double] => Double = l => l.sum / l.size
+  println(average(List(1, 2, 3)))
+
+  val ys = List(0, 1, 2, 3, 4)
+  println(ys dropRight 1)
+  println(ys take (ys.length - 1))
+}
