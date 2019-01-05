2c5de6b8b11e60188ba2f7c56b0fd98efd58eb07
Alex Baryzhikov <aleksiarts@gmail.com>
2019-01-05 16:59:13 +0300

    Feature: Recursive streams

--- ScalaSandbox/src/main/scala/tutorial/Streams.scala
+++ ScalaSandbox/src/main/scala/tutorial/Streams.scala
@@ -8,4 +8,13 @@ object Streams extends App {
     else lo #:: streamRange(lo + 1, hi)
 
   streamRange(0, 10).take(3).foreach(x => print(s"$x "))
+  println()
+
+  val str: Stream[Int] = 1 #:: str.map(_ + 1)
+  str.take(10).foreach(x => print(s"$x "))
+  println()
+
+  def fibs(a: Int = 1, b: Int = 1): Stream[Int] = a #:: fibs(b, a + b)
+
+  fibs().take(10).foreach(x => print(s"$x "))
 }
