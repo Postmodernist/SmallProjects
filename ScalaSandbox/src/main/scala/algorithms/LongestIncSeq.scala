596b0da8b59cbfccee287ea7daac1cc715ea048b
Alexey Baryzhikov <aleksiarts@gmail.com>
2019-01-07 01:15:26 +0300

    More lis functions

--- ScalaSandbox/src/main/scala/algorithms/LongestIncSeq.scala
+++ ScalaSandbox/src/main/scala/algorithms/LongestIncSeq.scala
@@ -1,28 +1,64 @@
 package algorithms
 
+import scala.collection.mutable
+
 object LongestIncSeq extends App {
-  def lis(xs: List[Int], x: Int = Int.MinValue): List[Int] = xs.filter(_ > x) match {
+  def myLis(xs: List[Int], x: Int = Int.MinValue): List[Int] = xs.filter(_ > x) match {
     case Nil => Nil
-    case h :: t => List(h :: lis(t, h), lis(t)).maxBy(_.size)
+    case h :: t => List(h :: myLis(t, h), myLis(t)).maxBy(_.size)
   }
 
-  def lisMemo(xs: List[Int], x: Int = Int.MinValue, memo: Map[List[Int], List[Int]] = Map()): List[Int] = {
+  def myLisMemo(xs: List[Int], x: Int = Int.MinValue, memo: Map[List[Int], List[Int]] = Map()): List[Int] = {
     xs.filter(_ > x) match {
       case Nil => Nil
       case ys if memo contains ys => memo.apply(ys)
       case h :: t =>
-        val res = List(h :: lisMemo(t, h, memo), lisMemo(t, memo = memo)).maxBy(_.size)
+        val res = List(h :: myLisMemo(t, h, memo), myLisMemo(t, memo = memo)).maxBy(_.size)
         memo + ((h :: t) -> res)
         res
     }
   }
 
-  println(lisMemo(List()))
-  println(lisMemo(List(1)))
-  println(lisMemo(List(1, 2, 4)))
-  println(lisMemo(List(1, 3, 2, 4)))
-  println(lisMemo(List(1, 3, 2, 3)))
-  println(lisMemo(List(5, 2, 4)))
-  println(lisMemo(List(4, 2, 1)))
-  println(lisMemo(List(7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1)))
+  /** Longest increasing sequence (recursive) */
+  def lisR(a: Array[Int], i: Int, t: mutable.Map[Int, Int] = mutable.Map()): Int = {
+    if (!t.contains(i)) {
+      t(i) = 1
+      for (j <- 0 until i; if a(j) < a(i)) {
+        t(i) = math.max(t(i), lisR(a, j, t) + 1)
+      }
+    }
+    t(i)
+  }
+
+  /** Longest increasing sequence (iterative) */
+  def lis(a: Array[Int]): List[Int] = {
+    val t = new Array[Int](a.length)
+    val prev = new Array[Int](a.length)
+    t.indices.foreach {
+      t(_) = 1
+      prev(_) = -1
+    }
+    for (i <- a.indices; j <- 0 until i; if a(j) < a(i) && t(i) < t(j) + 1) {
+      t(i) = t(j) + 1
+      prev(i) = j
+    }
+
+    // Reconstruct
+    var rec = List[Int]()
+    var current = t.indexOf(t.max)
+    while (current >= 0) {
+      rec = current :: rec
+      current = prev(current)
+    }
+    rec.map(a(_))
+  }
+
+  println(myLis(List(7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1)))
+  println(myLisMemo(List(7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1)))
+
+  val a = Array(7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1)
+  val T = mutable.Map[Int, Int]()
+  println(a.indices.map(lisR(a, _, T)).max)
+
+  println(lis(Array(7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1)))
 }
--- ScalaSandbox/src/test/scala/algorithms/LongestIncSeqTest.scala
+++ ScalaSandbox/src/test/scala/algorithms/LongestIncSeqTest.scala
@@ -1,28 +1,40 @@
 package algorithms
 
-import algorithms.LongestIncSeq.{lis, lisMemo}
+import algorithms.LongestIncSeq.{lis, lisR, myLis, myLisMemo}
 import org.scalatest.FunSuite
 
+import scala.collection.mutable
+
 class LongestIncSeqTest extends FunSuite {
-  test("LongestIncSeq.lis") {
-    assert(lis(List()) === List())
-    assert(lis(List(1)) === List(1))
-    assert(lis(List(1, 2, 4)) === List(1, 2, 4))
-    assert(lis(List(1, 3, 2, 4)) === List(1, 3, 4))
-    assert(lis(List(1, 3, 2, 3)) === List(1, 2, 3))
-    assert(lis(List(5, 2, 4)) === List(2, 4))
-    assert(lis(List(4, 2, 1)) === List(4))
-    assert(lis(List(7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1)) === List(2, 3, 4, 6, 9))
+  test("LongestIncSeq.myLis") {
+    assert(myLis(List()) === List())
+    assert(myLis(List(1)) === List(1))
+    assert(myLis(List(1, 2, 4)) === List(1, 2, 4))
+    assert(myLis(List(1, 3, 2, 4)) === List(1, 3, 4))
+    assert(myLis(List(1, 3, 2, 3)) === List(1, 2, 3))
+    assert(myLis(List(5, 2, 4)) === List(2, 4))
+    assert(myLis(List(4, 2, 1)) === List(4))
+    assert(myLis(List(7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1)) === List(2, 3, 4, 6, 9))
   }
 
-  test("LongestIncSeq.lisMemo") {
-    assert(lisMemo(List()) === List())
-    assert(lisMemo(List(1)) === List(1))
-    assert(lisMemo(List(1, 2, 4)) === List(1, 2, 4))
-    assert(lisMemo(List(1, 3, 2, 4)) === List(1, 3, 4))
-    assert(lisMemo(List(1, 3, 2, 3)) === List(1, 2, 3))
-    assert(lisMemo(List(5, 2, 4)) === List(2, 4))
-    assert(lisMemo(List(4, 2, 1)) === List(4))
-    assert(lisMemo(List(7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1)) === List(2, 3, 4, 6, 9))
+  test("LongestIncSeq.myLisMemo") {
+    assert(myLisMemo(List()) === List())
+    assert(myLisMemo(List(1)) === List(1))
+    assert(myLisMemo(List(1, 2, 4)) === List(1, 2, 4))
+    assert(myLisMemo(List(1, 3, 2, 4)) === List(1, 3, 4))
+    assert(myLisMemo(List(1, 3, 2, 3)) === List(1, 2, 3))
+    assert(myLisMemo(List(5, 2, 4)) === List(2, 4))
+    assert(myLisMemo(List(4, 2, 1)) === List(4))
+    assert(myLisMemo(List(7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1)) === List(2, 3, 4, 6, 9))
+  }
+
+  test("LongestIncSeq.lisR") {
+    val a = Array(7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1)
+    val t = mutable.Map[Int, Int]()
+    assert(a.indices.map(lisR(a, _, t)).max === 5)
+  }
+
+  test("LongestIncSeq.lis") {
+    assert(lis(Array(7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1)) === List(2, 3, 4, 6, 9))
   }
 }
