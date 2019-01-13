1b9f92c306798e3aa762c97dc80fe422bd690152
Alex Baryzhikov <aleksiarts@gmail.com>
2019-01-13 16:18:06 +0300

    More tests

--- ScalaSandbox/src/test/scala/algorithms/ParenthesesTest.scala
+++ ScalaSandbox/src/test/scala/algorithms/ParenthesesTest.scala
@@ -9,6 +9,7 @@ class ParenthesesTest extends FunSuite with BeforeAndAfter {
   val mul: (Int, Int) => Int = _ * _
 
   test("Parentheses.parentheses") {
+    assert(parentheses(Array(1, 5), List(add)) === 6)
     assert(parentheses(Array(5, 8, 7, 4, 8, 9), List(sub, add, mul, sub, add)) === 200)
   }
 }
