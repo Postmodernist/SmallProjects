43b16de7e5a0eed35750660ab08ff8b06c7edaad
Alex Baryzhikov <aleksiarts@gmail.com>
2019-01-26 15:36:14 +0300

    Improved qsort

--- ScalaSandbox/src/main/scala/Quicksort.scala
+++ ScalaSandbox/src/main/scala/Quicksort.scala
@@ -3,9 +3,10 @@ object Quicksort extends App {
   def qsort[T](l: List[T])(implicit ord: Ordering[T]): List[T] = l match {
     case Nil => Nil
     case x :: xs =>
-      val ys = xs.filter(ord.lteq(_, x))
-      val zs = xs.filter(ord.gt(_, x))
-      qsort(ys) ::: List(x) ::: qsort(zs)
+      val as = xs.filter(ord.lt(_, x))
+      val bs = x :: xs.filter(ord.eq(_, x))
+      val cs = xs.filter(ord.gt(_, x))
+      qsort(as) ::: bs ::: qsort(cs)
   }
 
   println(qsort(List(4, 3, 2, 1)))
