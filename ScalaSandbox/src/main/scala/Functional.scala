188534375a6e040fe95a178c9045154e2ca3e198
Alex Baryzhikov <aleksiarts@gmail.com>
2019-02-09 16:20:29 +0300

    Functor

--- ScalaSandbox/src/main/scala/Functional.scala
+++ ScalaSandbox/src/main/scala/Functional.scala
@@ -1,4 +1,5 @@
-object Functional extends App {
+object Functional1 extends App {
+
   val double: Int => Int = _ * 2
   val quad = double compose double
   (quad andThen println) (3)
@@ -16,9 +17,29 @@ object Functional extends App {
   println(ys dropRight 1)
   println(ys take (ys.length - 1))
 
-  val f = {
+  val f: PartialFunction[Int, Int] = {
     case x: Int if x % 2 == 0 => x
   }
   val a = Stream.from(0).collect(f).take(5).toList
   println(a)
+}
+
+
+object Functional2 extends App {
+
+  // Functor maps between categories preserving structure.
+  // A --f-> B => F[A] --F[f]-> F[B]
+
+  trait Functor[F[_]] {
+    def fmap[A, B](v: F[A])(implicit f: A => B): F[B]
+  }
+
+  implicit object ListFunctor extends Functor[List] {
+    override def fmap[A, B](v: List[A])(implicit f: A => B): List[B] = v match {
+      case Nil => Nil
+      case h :: t => f(h) :: fmap(t)(f)
+    }
+  }
+
+  println(ListFunctor.fmap(List("1", "2", "3")))
 }
