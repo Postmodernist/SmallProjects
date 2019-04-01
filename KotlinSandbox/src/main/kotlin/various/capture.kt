72e553e60039aa3f8fa2f21846d3e1165c48d2c7
Alex Baryzhikov <abaryzhikov@niisokb.ru>
2019-04-01 18:32:43 +0300

    Capturing

---
+++ KotlinSandbox/src/main/kotlin/various/capture.kt
@@ -0,0 +1,23 @@
+package various
+
+class Foo {
+    var baz: IntArray = intArrayOf(42)
+
+    fun bar(): () -> Unit {
+        var quux = intArrayOf(99)
+        val lambda = {
+            baz = intArrayOf(555)
+            println("lambda baz[0] = ${baz[0]}")
+            println("lambda quux[0] = ${quux[0]}")
+        }
+        quux = intArrayOf(666)
+        return lambda
+    }
+}
+
+fun main() {
+    val foo = Foo()
+    val qux = foo.bar()
+    qux()
+    println("foo baz[0] = ${foo.baz[0]}")
+}
