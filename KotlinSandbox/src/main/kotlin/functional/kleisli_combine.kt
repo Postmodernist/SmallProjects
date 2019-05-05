5b7cf13c1b10812a8c0563ba8e464b9d69160178
Alex Baryzhikov <aleksiarts@gmail.com>
2019-05-05 15:47:17 +0300

    Code cleanup

--- KotlinSandbox/src/main/kotlin/functional/kleisli_combine.kt
+++ KotlinSandbox/src/main/kotlin/functional/kleisli_combine.kt
@@ -2,15 +2,16 @@ package functional
 
 import kotlin.math.sqrt
 
-/* Data type Optional */
+/* Optional data type */
 class Opt<T>(val value: T? = null, val isValid: Boolean = false) {
     constructor(value: T) : this(value, true)
 }
 
 /* Composition */
 infix fun <A, B, C> ((B) -> Opt<C>).after(f: (A) -> Opt<B>): (A) -> Opt<C> = { x ->
+    val g = this
     val y = f(x)
-    if (!y.isValid) Opt(null, false) else this(y.value!!)
+    if (y.isValid) g(y.value!!) else Opt()
 }
 
 /* Identity */
@@ -21,13 +22,13 @@ val safeSqrt: (Double) -> Opt<Double> = { x -> if (x >= 0) Opt(sqrt(x)) else Opt
 val safeReciprocal: (Double) -> Opt<Double> = { x -> if (x != 0.0) Opt(1 / x) else Opt() }
 
 fun main() {
-    val sqrtReciprocal = safeSqrt after safeReciprocal
+    val sqrtOfReciprocal = safeSqrt after safeReciprocal
     val log: (Double) -> Unit = { x ->
-        val opt = sqrtReciprocal(x)
+        val opt = sqrtOfReciprocal(x)
         if (opt.isValid) println(opt.value) else println("oops...")
     }
 
-    log(42.0)
-    log(-42.0)
-    log(0.0)
+    log(42.0) // success
+    log(-42.0) // fails on safeSqrt
+    log(0.0) // fails on safeReciprocal
 }
