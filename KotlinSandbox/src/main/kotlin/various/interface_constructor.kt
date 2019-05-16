1e2a3ca357d919fbf3edb845037316125720c4d8
Alex Baryzhikov <abaryzhikov@niisokb.ru>
2019-05-16 13:29:10 +0300

    Interface constructor

--- KotlinSandbox/src/main/kotlin/various/interface_constructor.kt
+++ KotlinSandbox/src/main/kotlin/various/interface_constructor.kt
@@ -1,21 +1,19 @@
 package various
 
-object InterfaceConstructor {
+interface IConstructor {
+    fun bar(i: Int)
 
-    interface Foo {
-        fun bar(i: Int)
-
-        companion object {
-            operator fun invoke(f: (Int) -> Unit): Foo {
-                return object : Foo {
-                    override fun bar(i: Int) = f(i)
-                }
+    companion object {
+        operator fun invoke(f: (Int) -> Unit): IConstructor {
+            return object : IConstructor {
+                override fun bar(i: Int) = f(i)
             }
         }
     }
+}
 
-    fun main() {
-        println("Hello Kotlin!")
-        val foo = Foo { println(it) }
-    }
+fun main() {
+    println("Hello Kotlin!")
+    val foo = IConstructor { println(it) }
+    foo.bar(42)
 }
