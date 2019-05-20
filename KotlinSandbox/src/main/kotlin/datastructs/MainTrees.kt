a263d0bb76dad8aee816ba0c13857a4ea1e8f2d3
Alex Baryzhikov <aleksiarts@gmail.com>
2019-05-20 23:10:17 +0300

    RedBlackTree insert (bugged)

--- KotlinSandbox/src/main/kotlin/datastructs/MainTrees.kt
+++ KotlinSandbox/src/main/kotlin/datastructs/MainTrees.kt
@@ -3,6 +3,20 @@ package datastructs
 import java.util.*
 
 fun main() {
-    val a = TreeMap<String, Int>()
-    print(a)
+    val a = TreeMap<Int, String>()
+    a.put(41, "")
+    a.put(38, "")
+    a.put(31, "")
+    a.put(12, "")
+    a.put(19, "")
+    a.put(8, "")
+    println(a)
+    val b = RedBlackTree<Int>()
+    b.insert(41)
+    b.insert(38)
+    b.insert(31)
+    b.insert(12)
+    b.insert(19)
+    b.insert(8)
+    println(b)
 }
--- KotlinSandbox/src/main/kotlin/datastructs/RedBlackTree.kt
+++ KotlinSandbox/src/main/kotlin/datastructs/RedBlackTree.kt
@@ -1,11 +1,26 @@
 package datastructs
 
+import java.util.*
+
+/**
+ * Red-black tree is a BST with additional properties:
+ *
+ * 1) Each node is either red or black.
+ * 2) The root is black.
+ * 3) All leaves (NIL) are black.
+ * 4) If a node is red, then both its children are black.
+ * 5) Every path from a given node to any of its descendant NIL nodes contains the same number of black nodes.
+ */
 class RedBlackTree<T : Comparable<T>> {
 
     var size: Int = 0
         private set
 
     private var root: Node<T>? = null
+
+    fun insert(key: T) {
+        insert(Node(key))
+    }
 
     /** Assumes x.right is not null. */
     private fun rotateLeft(x: Node<T>) {
@@ -61,28 +76,37 @@ class RedBlackTree<T : Comparable<T>> {
     }
 
     private fun fixAfterInsertion(z: Node<T>) {
-        var x: Node<T> = z
+        var x: Node<T> = z // x is red
         var y: Node<T>?
+        // If x is a root, then the only violated property is 2, and we just make the root black.
+        // If x is not a root, then it has a parent.
         while (x != root && x.parent!!.color == RED) {
+            // The only property violated at this point is 4, because both x and its parent are red.
             val p: Node<T> = x.parent!!
             if (p === p.parent?.left) {
-                val pp: Node<T> = p.parent!!
+                // Since x.parent is red, it's not a root (root is black), and thus it has a parent.
+                val pp: Node<T> = p.parent!! // pp starts black
                 y = pp.right
                 if (y?.color == RED) {
+                    // Case 1: no matter if x is right or left child we do the same transformation
                     p.color = BLACK
                     y.color = BLACK
                     pp.color = RED
-                    x = pp
+                    x = pp // x is red again
                 } else {
                     if (x === p.right) {
-                        x = p
+                        // Case 2: rotate left immediately to transform the situation to Case 3
+                        x = p // x is red yet again
                         rotateLeft(x)
                     }
+                    // Case 3: some color changes and right rotation
                     p.color = BLACK
                     pp.color = RED
                     rotateRight(pp)
+                    // The while loop terminates because property 4 is satisfied
                 }
-            } else if (p === p.parent?.right) {
+            } else {
+                // This branch is symmetrical to the first branch.
                 val pp: Node<T> = p.parent!!
                 y = pp.left
                 if (y?.color == RED) {
@@ -104,11 +128,29 @@ class RedBlackTree<T : Comparable<T>> {
         root!!.color = BLACK
     }
 
+    override fun toString(): String {
+        return root?.showTree() ?: "null"
+    }
+
     private data class Node<V : Comparable<V>>(val key: V) {
         var parent: Node<V>? = null
         var left: Node<V>? = null
         var right: Node<V>? = null
         var color: Boolean = BLACK
+
+        fun showTree(): String {
+            val sb = StringBuilder()
+            val stack = ArrayDeque<Pair<Int, Node<V>>>()
+            stack.push(Pair(0, this))
+            while (!stack.isEmpty()) {
+                val (t, x) = stack.pop()
+                repeat(t) { sb.append("  ") }
+                sb.append("${x.key}-${if (x.color) "B" else "R"}\n")
+                if (x.left != null) stack.push(Pair(t + 1, x.left!!))
+                if (x.right != null) stack.push(Pair(t + 1, x.right!!))
+            }
+            return sb.toString()
+        }
     }
 
     private companion object {
