41d5809be7d20a8db0e2e3e2ecd0855802619be2
Alex Baryzhikov <abaryzhikov@niisokb.ru>
2019-08-06 16:07:50 +0300

    Cancelling coroutine

---
+++ KotlinSandbox/src/main/kotlin/coroutines/cancel_coroutine.kt
@@ -0,0 +1,21 @@
+package coroutines
+
+import kotlinx.coroutines.*
+
+fun main() {
+    val scope = CoroutineScope(Job() + Dispatchers.IO)
+    val job = scope.launch {
+        while (isActive) {
+            print(". ")
+            Thread.sleep(500)
+        }
+    }
+
+    runBlocking {
+        delay(2000)
+        scope.cancel()
+        job.join()
+    }
+
+    println("finished")
+}
