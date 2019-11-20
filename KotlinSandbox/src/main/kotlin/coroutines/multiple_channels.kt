c7028cd6a90f80093a1a678c22c7b621348d3cbe
Alex Baryzhikov <aleksiarts@gmail.com>
2019-11-20 22:13:37 +0300

    multiple channels: wait for receiver to finish receiving

--- KotlinSandbox/src/main/kotlin/coroutines/multiple_channels.kt
+++ KotlinSandbox/src/main/kotlin/coroutines/multiple_channels.kt
@@ -9,66 +9,83 @@ import kotlinx.coroutines.selects.select
 import kotlin.random.Random
 
 class DataSink {
-    val intChannel = Channel<Int>(BUFFERED)
-    val stringChannel = Channel<String>(BUFFERED)
+    val intChannel = Channel<Data<Int>>(BUFFERED)
+    val stringChannel = Channel<Data<String>>(BUFFERED)
+
     val eventLoop = CoroutineScope(Dispatchers.Default).launch {
-        withTimeout(1000) {
-            eventLoop(intChannel, stringChannel)
-        }
+        eventLoop(intChannel, stringChannel)
     }
 
     private suspend fun eventLoop(
-            intSource: ReceiveChannel<Int>,
-            stringSource: ReceiveChannel<String>
+            intSource: ReceiveChannel<Data<Int>>,
+            stringSource: ReceiveChannel<Data<String>>
     ) {
-        while (true) {
+        var intSourceOpen = true
+        var stringSourceOpen = true
+        while (intSourceOpen || stringSourceOpen) {
             select<Unit> {
-                stringSource.onReceive { println("received string: $it"); delay(200) }
-                intSource.onReceive { println("received int: $it"); delay(200) }
+                stringSource.onReceive { stringSourceOpen = receive(it) }
+                intSource.onReceive { intSourceOpen = receive(it) }
+            }
+        }
+    }
+
+    private suspend fun <T> receive(data: Data<T>): Boolean {
+        return when (data) {
+            is Data.Value -> {
+                println("received ${data.value}")
+                delay(Random.nextLong(100, 400))
+                true
+            }
+            is Data.Close -> {
+                false
             }
         }
     }
 }
 
+sealed class Data<T> {
+    class Value<T>(val value: T) : Data<T>()
+    class Close<T> : Data<T>()
+}
+
 fun main() {
     runBlocking {
         val dataSink = DataSink()
-        val jobs = listOf(
-                generateInts(dataSink.intChannel),
-                generateStrings(dataSink.stringChannel)
-        )
-        jobs.joinAll()
+        listOf(
+                generate("INT", 10, 4.0, dataSink.intChannel) {
+                    Random.nextInt(100, 1000)
+                },
+                generate("STR", 5, 1.5, dataSink.stringChannel) {
+                    randomString()
+                }
+        ).joinAll()
         dataSink.eventLoop.join()
     }
 }
 
-private fun CoroutineScope.generateInts(sink: SendChannel<Int>): Job {
+private fun <T> CoroutineScope.generate(
+        name: String,
+        n: Int,
+        rate: Double,
+        sink: SendChannel<Data<T>>,
+        supplier: () -> T
+): Job {
     return launch {
-        repeat(10) {
-            delay(Random.nextLong(300))
-            val x = Random.nextInt(1000)
-            println("sending: $x")
-            sink.send(x)
+        repeat(n) {
+            delay((1000 / rate).toLong())
+            val x = supplier()
+            println("$name sending $x")
+            sink.send(Data.Value(x))
         }
-        println("generateInts finished")
+        sink.send(Data.Close())
+        println("$name finished")
     }
 }
 
-private fun CoroutineScope.generateStrings(sink: SendChannel<String>): Job {
-    return launch {
-        repeat(5) {
-            delay(Random.nextLong(1000))
-            val s = randomString()
-            println("sending: $s")
-            sink.send(s)
-        }
-        println("generateStrings finished")
-    }
-}
-
-fun randomString(): String {
+private fun randomString(): String {
     val sb = StringBuilder()
-    repeat(Random.nextInt(4, 10)) {
+    repeat(3) {
         sb.append(Random.nextInt('a'.toInt(), 'z'.toInt()).toChar())
     }
     return sb.toString()
